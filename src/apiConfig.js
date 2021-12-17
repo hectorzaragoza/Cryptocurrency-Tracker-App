6ac1528819a3f2d8aa889d3a966080ad61d7ee6c
harrdev <jasonharrphx@live.com>
2021-12-17 13:09:21 -0700

    fixed apiURL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://crypto-bytes.herokuapp.com/',
+	production: 'https://crypto-bytes.herokuapp.com',
 	development: 'http://localhost:8000',
 }
 
