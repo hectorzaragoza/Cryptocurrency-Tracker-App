3775ee2f0d8e880df3f943a03238ec32bd8e7f6e
harrdev <jasonharrphx@live.com>
2021-12-19 15:36:04 -0700

    changed styling on contact images

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,6 @@ import github from '../images/github.png'
 const Contact = (props) => {
     // const { msgAlert, user } = props
     console.log('props in home', props)
-    // CHANGE INLINE STYLING WHEN STYLING PAGE!!!
     return (
         <div className="contact">
             <div id="contactHeader">
@@ -17,24 +16,24 @@ const Contact = (props) => {
             </div>
             <div class="row">
                 <div className="col-sm-3">
-                    <img src={jason} height="250px" width="250px" alt="Jason"></img><br></br><br></br><h5>Jason Harr</h5>
+                    <img className="contactImages" src={jason} alt="Jason"></img><br></br><br></br><h5>Jason Harr</h5>
                     <div className="logoDiv">
                         <a href="https://www.linkedin.com/in/jasonharr/" target="_blank" rel="noreferrer"><img className="logo" src={linkedin}></img></a><br></br><br></br>
                         <a href="https://github.com/harrdev" target="_blank" rel="noreferrer"><img className="logo" src={github}></img></a>
                     </div>
                 </div>
                 <div className="col-sm-3">
-                    <img src={hector} height="250px" width="250px" alt="Hector"></img><br></br><br></br><h5>Hector Zaragoza</h5>
+                    <img className="contactImages" src={hector} alt="Hector"></img><br></br><br></br><h5>Hector Zaragoza</h5>
                     <a href="https://www.linkedin.com/in/hectorzaragoza1/" target="_blank" rel="noreferrer"><img className="logo" src={linkedin}></img></a><br></br><br></br>
                     <a href="https://github.com/hectorzaragoza" target="_blank" rel="noreferrer"><img className="logo" src={github}></img></a>
                 </div>
                 <div className="col-sm-3">
-                    <img src={emily} height="250px" width="250px" alt="Emily"></img><br></br><br></br><h5>Emily Barwinczak</h5>
+                    <img className="contactImages" src={emily} alt="Emily"></img><br></br><br></br><h5>Emily Barwinczak</h5>
                     <a href="https://www.linkedin.com/in/emilybarwinczak/" target="_blank" rel="noreferrer"><img className="logo" src={linkedin}></img></a><br></br><br></br>
                     <a href="https://github.com/emilybarwinczak" target="_blank" rel="noreferrer"><img className="logo" src={github}></img></a>
                 </div>
                 <div className="col-sm-3">
-                    <img src={steven} height="250px" width="250px" alt="Steven"></img><br></br><br></br><h5>Steven Lopez</h5>
+                    <img className="contactImages" src={steven} height="250px" width="250px" alt="Steven"></img><br></br><br></br><h5>Steven Lopez</h5>
                     <a href="https://www.linkedin.com/in/steven-lopez1/" target="_blank" rel="noreferrer"><img className="logo" src={linkedin}></img></a><br></br><br></br>
                     <a href="https://github.com/steven43533" target="_blank" rel="noreferrer"><img className="logo" src={github}></img></a>
                 </div>
